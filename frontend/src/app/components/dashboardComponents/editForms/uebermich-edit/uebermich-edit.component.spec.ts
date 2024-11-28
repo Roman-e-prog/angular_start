2eee259acaaa86ecb3d5a29d2bd887dac2e2d865
Roman-e-prog <roman.rostock@googlemail.com>
2024-11-28 10:16:26 +0100

    no changes

--- frontend/src/app/components/dashboardComponents/editForms/uebermich-edit/uebermich-edit.component.spec.ts
+++ frontend/src/app/components/dashboardComponents/editForms/uebermich-edit/uebermich-edit.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UebermichEditComponent } from './uebermich-edit.component';
+import { NgIconsModule, provideIcons } from '@ng-icons/core';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 describe('UebermichEditComponent', () => {
   let component: UebermichEditComponent;
   let fixture: ComponentFixture<UebermichEditComponent>;
-
+  let mockStore: MockStore;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UebermichEditComponent]
+      imports: [
+        UebermichEditComponent,
+        NgIconsModule,
+      ],
+      providers:[
+        provideIcons({}),
+        provideMockStore()
+      ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(UebermichEditComponent);
     component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  // fit('should send the editData', ()=>{
+  //   const editUebermichForm = fixture.debugElement.nativeElement.querySelector('.editUebermichForm');
+  //   const my_person = fixture.debugElement.nativeElement.querySelector('#my_person');
+  //   const mainButton = fixture.debugElement.nativeElement.querySelector('#mainButton');
+  // })
 });
