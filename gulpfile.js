55204f677a483e47c8ca36631252727898ccf4e5
Roman-e-prog <roman.rostock@googlemail.com>
2024-12-13 10:17:10 +0100

    changed structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,83 @@
 const gulp = require('gulp');
-const clean = require('gulp-clean');
-const styles = require('./gulp-tasks/styles');
-const scripts = require('./gulp-tasks/scripts');
-const env = require('gulp-env');
 const nodemon = require('gulp-nodemon');
+const backendscripts = require('./gulp-tasks/backendscripts');
+const scripts = require('./gulp-tasks/scripts');
+const styles = require('./gulp-tasks/styles');
+const clean = require('gulp-clean');
 const replace = require('gulp-replace');
 const htmlmin = require('gulp-htmlmin'); 
 const rename = require('gulp-rename');
 const useref = require('gulp-useref');
-const dotenv = require('dotenv');
-const backendscripts = require('./gulp-tasks/backendscripts');
-const { exec } = require('child_process');
-dotenv.config();
-gulp.task('log', function(done) {
-	console.log('Gulp is running');
-	done();
-});
-gulp.task('clean', () => {
-	console.log('Running clean task');
-	return gulp.src('frontend/build', { allowEmpty: true })
-		.pipe(clean());
-});
-gulp.task('backendscripts', backendscripts);
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
+const htmlFiles = require('./gulp-tasks/html');
+const updateTemplatePaths = require('./gulp-tasks/updateTemplateUrl');
+const updateStylePaths = require('./gulp-tasks/updateStyleUrl');
+const moveAssets = require('./gulp-tasks/moveAssets');
 
-gulp.task('update-import-paths', () => {
-	console.log('Running update-import-paths task');
-	return gulp.src('frontend/build/js/**/*.js')
-	  .pipe(replace(/\.scss/g, '.css'))
-	  .pipe(gulp.dest('frontend/build/js'))
-	  .on('data', (file) => {
-		  console.log(`Processed file: ${file.path}`);
-	  });
+
+// Utilities to navigate directories
+function navigateToFrontend(cb) {
+    process.chdir('frontend');
+    cb();
+}
+
+function navigateBack(cb) {
+    process.chdir('../');
+    cb();
+}
+
+// Frontend tasks
+gulp.task('clean-frontend', () => {
+    return gulp.src('./build', { allowEmpty: true })
+        .pipe(clean());
 });
-gulp.task('move-html', function() {
-	return gulp.src('frontend/src/*.html')
-	  .pipe(useref())
-	  .pipe(htmlmin({ collapseWhitespace: true }))
-	  .pipe(rename('index.html'))
-	  .pipe(gulp.dest('frontend/build/'));
-  });
-  
+gulp.task('move-indexHtml', function() {
+    return gulp.src('./src/index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./build')); // Moves to the root of the build folder
+});
+gulp.task('move-favicon-icon', function(){
+    return gulp.src('./src/favicon.ico') // Path to favicon.ico
+    .pipe(gulp.dest('./build'));
+})
+gulp.task('move-uploads-folder', function(){
+    return gulp.src('./src/uploads/**/*', { allowEmpty: true }) // Copy uploads folder
+    .pipe(gulp.dest('./build/uploads'));
+})
+
+gulp.task('frontend-build', gulp.series(
+    navigateToFrontend,
+    'clean-frontend',
+    styles,
+    htmlFiles,
+    scripts,
+    updateStylePaths,
+    updateTemplatePaths,
+    moveAssets,
+    'move-indexHtml',
+    'move-favicon-icon',
+    'move-uploads-folder',
+    navigateBack
+));
+
+// Backend tasks
 gulp.task('nodemon', function(done) {
-	nodemon({
-	  script: 'backend/index.ts',
-	  ext: 'ts',
-	  env: { 'NODE_ENV': 'production', 'DB_USER': 'postgres' , 'DB_HOST': 'postgres' }, 
-	  done: done
-	});
-  });
-
-
-gulp.task('watch', function() {
-	gulp.watch('frontend/src/**/*.scss', gulp.series('styles'));
-	gulp.watch('frontend/src/**/*.jsx', gulp.series('scripts'));
-	gulp.watch('backend/**/*.js', gulp.series('backendscripts'));
+    nodemon({
+        script: 'backend/index.ts',
+        ext: 'ts',
+        env: { 'NODE_ENV': 'production', 'DB_USER': 'postgres', 'DB_HOST': 'postgres' },
+        done: done
+    });
 });
 
-gulp.task('frontend-build', gulp.series('log', 'clean', 'styles', 'scripts', 'update-import-paths', 'move-html'));
-gulp.task('backend-build', gulp.series('log', 'backendscripts', 'nodemon'));
+gulp.task('backend-build', gulp.series(backendscripts, 'nodemon'));
+
+// Watch tasks
+gulp.task('watch', function() {
+    gulp.watch('frontend/src/**/*.scss', gulp.series(styles));
+    gulp.watch('frontend/src/**/*.html', gulp.series(htmlFiles));
+    gulp.watch('frontend/src/**/*.ts', gulp.series(scripts));
+    gulp.watch('backend/**/*.ts', gulp.series(backendscripts));
+});
+
+// Default task
 gulp.task('default', gulp.series('frontend-build', 'backend-build', 'watch'));
-		
