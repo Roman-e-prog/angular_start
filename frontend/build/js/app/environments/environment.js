c8ee6aac7ce3f750b6e4db6f07deeb14b74f6101
Roman-e-prog <roman.rostock@googlemail.com>
2024-12-14 13:31:01 +0100

    gulp new run

--- frontend/build/js/app/environments/environment.js
+++ frontend/build/js/app/environments/environment.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.environment=void 0;const environment=exports.environment={production:!1};
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.environment=void 0;const environment=exports.environment={production:!1,apiURL:"http://localhost:5000/"};
